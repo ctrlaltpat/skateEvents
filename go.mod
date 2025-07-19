8fedfdd888f2a1b216beb53cf5dc68f868772914
Patrick <ctrlaltpat@gmail.com>
2025-07-19 18:37:19 +0100

    feat: basic http (déjà vu?)

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module ctrlaltpat/myskatesgo
+module github.com/ctrlaltpat/my-skates-go
 
 go 1.24.1
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+var port = ":5178"
 
 func main() {
-	fmt.Println("Another attempt")
+	server := &http.Server{
+		Addr:    port,
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	fmt.Println("Server is running on http://localhost" + port)
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error listening to server:", err)
+		return
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello, World!")
+	w.Write([]byte("Hello, World!"))
 }
