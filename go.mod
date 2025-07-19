13fefa80b15b07f8901a164cccc2110513eba9b6
Patrick <ctrlaltpat@gmail.com>
2025-07-19 18:43:32 +0100

    feat: add chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ctrlaltpat/my-skates-go
 
 go 1.24.1
+
+require github.com/go-chi/chi/v5 v5.2.2
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 var port = ":5178"
